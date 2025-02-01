2f51da326ba8cb2de6b101361a536bbe0793b61e
Joan Gilabert Bataller <me.joang@gmail.com>
2025-02-01 11:54:16 +0100

    Inicial

---
+++ js/logica.js
@@ -0,0 +1,13 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-XXXXXXXXX-X');
+
+function irA(destino) {
+    const rutaActual = window.location.href;
+    if (rutaActual.includes('productos')) {
+        window.location.href = destino === 'index' ? '../index.html' : `${destino}.html`;
+    } else {
+        window.location.href = destino === 'index' ? 'index.html' : `productos/${destino}.html`;
+    }
+}
