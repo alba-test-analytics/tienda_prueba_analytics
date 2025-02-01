e93480fe5a2ef7ca31febbe779ea5a7f61113fcf
Joan Gilabert Bataller <me.joang@gmail.com>
2025-02-01 12:32:06 +0100

    script analytics en index

--- js/logica.js
+++ js/logica.js
@@ -1,8 +1,3 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-FBJFKJY6N3');
-
 function irA(destino) {
     const rutaActual = window.location.href;
     if (rutaActual.includes('productos')) {
